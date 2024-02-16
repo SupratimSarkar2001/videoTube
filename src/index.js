992eddd9ffeb7c7ab67856b34b72a917d856ed75
Supratim <supratimsarkar1711@gmail.com>
2024-02-16 10:17:21 +0530

    INIT: express app()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js'
+import { app } from './app.js';
 dotenv.config({
  path:'./env'
 })
 
-connectDB();
+connectDB().
+then(()=>{
+ app.listen(process.env.PORT || 8000, ()=>{
+   console.log(`Server listening on port ${process.env.PORT || 8000}`);
+ })
+}).
+catch((error)=>{
+  console.log("ERROR : Failed to connect to DB", error);
+})
